b8462995711b57a2e360c487460f6d61c9e255a9
KyrylIeremieiev <34677@ma-web.nl>
2023-11-25 00:31:51 +0100

    make a start

--- bmv/src/App.tsx
+++ bmv/src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Logo from './img/logo-gemeente-amsterdam.png'
 function App() {
   return (
     <>
-      <footer className='header'></footer>
+      <header className='header'>
+        <img src={Logo} alt="img not found" className='header__image' />
+        <p className='header__text'>Menu</p>
+      </header>
       <main className='main'>
         <section className='welcome'>
-          
+          <article className='text'>
+            <h1 className='welcome__title'>
+              Bouwmanagement en <br></br>Maatschappelijk Vastgoed
+            </h1>
+          </article>
+        
         </section>
+        <section className='intro'></section>
       </main>
     </>
   );
