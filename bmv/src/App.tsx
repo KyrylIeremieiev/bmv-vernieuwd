87540d11d8b5c3feeec2fe239a1569b6fa3ecb45
KyrylIeremieiev <34677@ma-web.nl>
2023-11-24 12:59:01 +0100

    start project

---
+++ bmv/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <footer className='header'></footer>
+      <main className='main'>
+        <section className='welcome'>
+          
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
